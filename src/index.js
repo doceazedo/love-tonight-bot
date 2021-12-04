072d1707636a664b251e2577744c23c8ae65421e
DoceAzedo <oi@lucasfernandes.com.br>
2021-12-03 22:26:41 -0300

    fix !hino, auto !sr, very useful commented stuff

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 let didPlay = false;
+let requested = false;
 const keywords = ['tocou', 'love', 'tonight'];
 const trueMsg = 'sim, já tocou Love tonight hoje!!!1! 😎👍';
 const falseMsg = 'ainda não tocou Love Tonight hoje 😭';
@@ -23,7 +24,7 @@ client.on('message', (channel, tags, message, self) => {
   if (tags.username == 'moniquelive_bot' && message.toLowerCase().startsWith('shouse - love tonight')) {
     didPlay = true;
     client.say(channel, 'Atenção, @doceazedo911! Está tocando Love Tonight catJAM');
-    client.say(channel, '!hino');
+    setTimeout(() => client.say(channel, '!hino'), 1500);
     return;
   }
 
@@ -33,5 +34,31 @@ client.on('message', (channel, tags, message, self) => {
   });
   if (keywordsFound < keywords.length) return;
 
-  client.say(channel, `@${tags.username} ${didPlay ? trueMsg : falseMsg}`);
+  client.say(channel, `@${tags.username} ${didPlay ? trueMsg : falseMsg} ${!didPlay && !requested ? '(mas jaja toca! 🤫)' : ''}`);
+  if (!didPlay && !requested) {
+    requested = true;
+    setTimeout(() => {
+      client.say(channel, '!sr https://open.spotify.com/track/6OufwUcCqo81guU2jAlDVP?si=2b1947f7d8124b7d');
+    }, 2000);
+  }
 });
+
+// setTimeout(() => {
+//   client.say('moniquelive', '⬛ ⬛ ⬛ ⬛ gopherPls ⬛ ⬛ ⬛ ⬛');
+//   setTimeout(() => {
+//     client.say('moniquelive', '⬛ ⬛ ⬛ gopherPls gopherPls gopherPls ⬛ ⬛ ⬛');
+//     setTimeout(() => {
+//       client.say('moniquelive', '⬛ ⬛ gopherPls gopherPls gopherPls gopherPls gopherPls ⬛ ⬛');
+//       setTimeout(() => {
+//         client.say('moniquelive', '⬛ gopherPls gopherPls gopherPls gopherPls gopherPls gopherPls gopherPls ⬛');
+//         setTimeout(() => {
+//           client.say('moniquelive', 'gopherPls gopherPls gopherPls gopherPls gopherPls gopherPls gopherPls gopherPls gopherPls');
+//         }, 1100);
+//       }, 1100);
+//     }, 1100);
+//   }, 1100);
+// }, 2000);
+
+// setTimeout(() => {
+//   client.say('moniquelive', '!xo');
+// }, 2000);
